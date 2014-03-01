f62fe2fe66b91fefa74626bbb86a9466469a0eae
John Wu <webmaster@leapoahead.com>
2014-03-01 09:01:42 +0800

    EL

---
+++ EL/src/java/com/wudi/models/User.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.wudi.models;
+
+/**
+ *
+ * @author john
+ */
+public class User {
+    private String name;
+    
+    public User() {
+        name = "";
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+}
